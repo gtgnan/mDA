546ae5e99dc11e1c7cd4c4bff87747ef25c13c9d
GaryChenMesser <gary9192631770@gmail.com>
2024-04-09 23:12:47 -0400

    Update for path issues.

--- Notebook/.ipynb_checkpoints/mDA_Shin2017B-checkpoint.py
+++ Notebook/.ipynb_checkpoints/mDA_Shin2017B-checkpoint.py
@@ -203,7 +203,7 @@ n_subjects = len(subjects)
 n_sessions = len(sessions)
 n_runs = len(runs)
 
-print(f"Loading subjects {subjects}...")
+print(f"Loading subjects {subjects} to {args.download_path}...")
 if args.download_path:
     data = dataset1.get_data(subjects=subjects, cache_config={'path': args.download_path})
 else:
